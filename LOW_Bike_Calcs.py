d1b01d1f0d3a219d08c833e664ebbdef3dd233e5
Thomas Wilbur <twilbur@umich.edu>
2020-08-09 13:38:41 -0700

    Added a newline in text files

--- LOW_Bike_Calcs.py
+++ LOW_Bike_Calcs.py
@@ -56,8 +56,8 @@ def write_time_splits_names(mins_in, secs_in, split_names, file_name):
     file_name = file_name+".txt"
     with open(file_name, 'w') as f:
 
-        f.write(file_name+": \n")
-        f.write("Split Time\tSplit Name")
+        f.write(file_name+": \n\n")
+        f.write("Split Time\tSplit Name\n")
         for counter in range(len(split_names)):
             sec_string = str(secs_in[counter]) if secs_in[counter]>9 else "0"+str(secs_in[counter])
             temp_time = str(mins_in[counter])+":"+sec_string
